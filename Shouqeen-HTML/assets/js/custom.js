90557b9e8f32ad05bb8f69090c289ff669a9dc0b
srinivasbuddala <srinivaswantit@gmail.com>
2022-11-18 14:33:16 +0530

    Home page has done
    Home page has done

---
+++ Shouqeen-HTML/assets/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.selectedcat').on('click', function(){
+        $('.category-list').toggleClass('active');
+    });
+    $('.category-search ul.category-list > li a').on('click',function(){
+        $('.category-list').removeClass('active');
+    });
+    $('.user-menu > a').on('click', function(){
+        $(this).siblings('.user-menu-list').fadeToggle();
+    })
+    $('..user-menu-list  a').on('click', function(){
+        $('.user-menu-list').hide();
+    })
+});
